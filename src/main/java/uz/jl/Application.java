6641439a9f37a10fb8e48cf267cf76aa8dcdfad5
Nodirbeke <nodirbekjuraev02@gmail.com>
2022-01-27 14:19:14 +0500

    initial commit2

--- src/main/java/uz/jl/Application.java
+++ src/main/java/uz/jl/Application.java
@@ -1,15 +1,9 @@
 package uz.jl;
 
 import uz.jl.configs.ApplicationContextHolder;
-import uz.jl.security.SecurityHolder;
 import uz.jl.ui.AuthUI;
 import uz.jl.ui.MenuUI;
 import uz.jl.ui.UserUI;
-import uz.jl.utils.Color;
-import uz.jl.utils.Input;
-import uz.jl.utils.Print;
-
-import java.util.Objects;
 
 public class Application {
     private static final MenuUI menu = ApplicationContextHolder.getBean(MenuUI.class);
@@ -21,20 +15,7 @@ public class Application {
     }
 
     private static void run() {
-        String opt = Input.getStr();
-        if (Objects.isNull(SecurityHolder.user)) {
-//            menu.authMenu();
-            if (opt.equals("1")) {
-                authUI.login();
-            } else if (opt.equals("2")) {
-                userUI.create();
-            }
-        }
-        if (opt.equals("0")) {
-            Print.println(Color.RED, "Bopti");
-            return;
-        }
-        run();
+        MenuUI.menu();
     }
 
 }
--- src/main/java/uz/jl/ui/MenuUI.java
+++ src/main/java/uz/jl/ui/MenuUI.java
@@ -1,14 +1,78 @@
 package uz.jl.ui;
 
+import uz.jl.configs.ApplicationContextHolder;
+import uz.jl.security.SecurityHolder;
 import uz.jl.utils.Color;
 import uz.jl.utils.Input;
 import uz.jl.utils.Print;
 
+import java.util.Objects;
+
 public class MenuUI {
-    public void authMenu() {
-        Print.println(Color.BLUE, "1.Login");
-        Print.println(Color.BLUE, "2.Register");
-        Print.println(Color.BLUE, "3.Exit");
+    private static final AuthUI authUI = ApplicationContextHolder.getBean(AuthUI.class);
+    private static final UserUI userUI = ApplicationContextHolder.getBean(UserUI.class);
+
+    public static void menu() {
+        if (Objects.isNull(SecurityHolder.user)) {
+            Print.println(Color.BLUE, "1. Login");
+            Print.println(Color.BLUE, "2. Register");
+            Print.println(Color.BLUE, "0. Exit");
+
+            String str = Input.getStr("Enter choice: ");
+
+            switch (str) {
+                case "1" -> authUI.login();
+                case "2" -> userUI.create();
+                case "0" -> {
+                    Print.println(Color.YELLOW, "Bye");
+                    return;
+                }
+                default -> Print.println(Color.RED, "Wrong choice");
+            }
+        } else {
+            switch (SecurityHolder.user.getRole()) {
+                case STUDENT:
+                    studentMenu();
+                    break;
+                case TEACHER:
+
+                    break;
+                case ADMIN:
+
+                    break;
+            }
+        }
+        menu();
+    }
+
+    private static void studentMenu() {
+
+        Print.println("1. Quizzes");
+        Print.println("2. Solved Quizzes");
+        Print.println("3. Settings ");
+        Print.println("0. Logout");
+
+        String str = Input.getStr("Enter choice: ");
+
+        switch (str) {
+//            case "1" -> quizzesMenu();
+            case "2" -> allSolvedQuizzesMenu();
+            case "3" -> settingsMenu();
+            case "0" -> {
+                SecurityHolder.killUser();
+            }
+            default -> {
+                Print.println(Color.RED, "Wrong choice");
+                studentMenu();
+            }
+        }
+    }
+
+    private static void settingsMenu() {
+
+    }
+
+    private static void allSolvedQuizzesMenu() {
 
     }
 
